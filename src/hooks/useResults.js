d026b75895dc1fd1347457278f4e599d82ed7743
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-07-25 11:45:39 -0700

    Added Caching for search result

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,20 +1,34 @@
 import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async (searchTerm) => {
+        setErrorMessage('');
         try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-            setResults(response.data.businesses);
+            let results;
+            searchTerm = searchTerm ? searchTerm : " ";
+            const jsonValue = await AsyncStorage.getItem(searchTerm);
+            
+            if (jsonValue === null) {
+                const response = await yelp.get('/search', {
+                    params: {
+                        limit: 50,
+                        term: searchTerm,
+                        location: 'Portland'
+                    }
+                });
+                results = response.data.businesses;
+                stringValue = JSON.stringify(results);
+                await AsyncStorage.setItem(searchTerm, stringValue);
+            } else {
+                results = JSON.parse(jsonValue);
+            }
+
+            setResults(results);
         } catch (err) {
             setErrorMessage('Something went wrong');
         }
